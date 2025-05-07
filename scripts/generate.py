d4b2ef040f985a0fb8a5eed774e97b3c7447b8aa
ayildiri <95155708+ayildiri@users.noreply.github.com>
2025-05-06 20:27:50 -0700

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -29,7 +29,9 @@ if device.type == 'cuda':
     print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3, 1), 'GB')
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("--augment", help="options: augmented, original", default="augmented", choices=["augmented", "original"])
+parser.add_argument("--alpha", default="fixed", choices=["fixed","schedule"])  # Added alpha parameter
 parser.add_argument("--tokenization", help="options: oldtok, RT_tokenized", default="oldtok", choices=["oldtok", "RT_tokenized"])
 parser.add_argument("--embedding_dim", help="latent dimension (equals word embedding dimension in this model)", default=32)
 parser.add_argument("--beta", default=1, help="option: <any number>, schedule", choices=["normalVAE","schedule"])
@@ -43,6 +45,7 @@ parser.add_argument("--dec_layers", type=int, default=4)
 parser.add_argument("--max_beta", type=float, default=0.1)
 parser.add_argument("--max_alpha", type=float, default=0.1)
 parser.add_argument("--epsilon", type=float, default=1)
+parser.add_argument("--save_dir", type=str, required=True, help="Path to save model and generated results")
 
 
 
@@ -67,8 +70,8 @@ num_edge_features = dict_test_loader['0'][0].num_edge_features
 
 # Load model
 # Create an instance of the G2S model from checkpoint
-model_name = 'Model_'+data_augment+'data_DecL='+str(args.dec_layers)+'_beta='+str(args.beta)+'_maxbeta='+str(args.max_beta)+'_maxalpha='+str(args.max_alpha)+'eps='+str(args.epsilon)+'_loss='+str(args.loss)+'_augment='+str(args.augment)+'_tokenization='+str(args.tokenization)+'_AE_warmup='+str(args.AE_Warmup)+'_init='+str(args.initialization)+'_seed='+str(args.seed)+'_add_latent='+str(add_latent)+'_pp-guided='+str(args.ppguided)+'/'
-filepath = os.path.join(main_dir_path,'Checkpoints/', model_name,"model_best_loss.pt")
+model_name = 'Model_'+data_augment+'data_DecL='+str(args.dec_layers)+'_beta='+str(args.beta)+'_alpha='+str(args.alpha)+'_maxbeta='+str(args.max_beta)+'_maxalpha='+str(args.max_alpha)+'eps='+str(args.epsilon)+'_loss='+str(args.loss)+'_augment='+str(args.augment)+'_tokenization='+str(args.tokenization)+'_AE_warmup='+str(args.AE_Warmup)+'_init='+str(args.initialization)+'_seed='+str(args.seed)+'_add_latent='+str(add_latent)+'_pp-guided='+str(args.ppguided)+'/'filepath = os.path.join(args.save_dir, model_name, "model_best_loss.pt")
+
 if os.path.isfile(filepath):
     if args.ppguided:
         model_type = G2S_VAE_PPguided
@@ -76,7 +79,7 @@ if os.path.isfile(filepath):
         model_type = G2S_VAE_PPguideddisabled
     checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
     model_config = checkpoint["model_config"]
-    batch_size = model_config['batch_size']
+    batch_size = model_config.get('batch_size', 64)
     hidden_dimension = model_config['hidden_dimension']
     embedding_dimension = model_config['embedding_dim']
     model_config["max_alpha"]=args.max_alpha
@@ -91,7 +94,7 @@ if os.path.isfile(filepath):
     model.to(device)
 
     # Directory to save results
-    dir_name=  os.path.join(main_dir_path,'Checkpoints/', model_name)
+    dir_name = os.path.join(args.save_dir, model_name)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
